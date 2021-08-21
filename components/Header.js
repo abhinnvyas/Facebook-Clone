dcca29bd82db021f65708d276da11df6348f1f9c
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-08-20 21:21:51 -0700

    Layed out the fundamental Structure of this build

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      {/* Left */}
+      {/* Center */}
+      {/* Right */}
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,7 +7,15 @@ export default function Home() {
       <Head>
         <title>Facebook</title>
       </Head>
-      <h1>Lets build Facebook</h1>
+
+      {/* Header */}
+      <Header />
+
+      <main>
+        {/* SideBar */}
+        {/* Feed */}
+        {/* Widget */}
+      </main>
     </div>
   );
 }
