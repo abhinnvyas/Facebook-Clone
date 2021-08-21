52c391d4f22ee504cf38208bcd067d7d888d2a65
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-08-21 16:15:34 -0700

    building the header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,28 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
+
 function Header() {
   return (
     <div>
       <h1>Header</h1>
+
       {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+
+        <div className="flex">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+
       {/* Center */}
+
       {/* Right */}
     </div>
   );
