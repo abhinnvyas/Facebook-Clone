6547967d340d22593a7368cc80b7ec56049f6a8b
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-08-25 13:42:21 -0700

    Successfully built the Story Component

--- components/Header.js
+++ components/Header.js
@@ -66,7 +66,7 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <p className="hidden sm:inline whitespace-nowrap pr-3 font-semibold">
+        <p className="hidden lg:inline-flex text-sm whitespace-nowrap pr-3 font-semibold">
           {session.user.name}
         </p>{" "}
         {/* User's Name */}
